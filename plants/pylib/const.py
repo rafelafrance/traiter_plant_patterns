813e2a28ecf5323a26d8d14f2f80be252fb57f7b
rafelafrance <raphael.lafrance@gmail.com>
2023-02-14 15:12:31 -0500

    Add a symbolic link to the taxa database

--- plants/pylib/const.py
+++ plants/pylib/const.py
@@ -7,15 +7,15 @@ VOCAB_DIR = Path(vocab.__file__).parent
 TERM_DB = VOCAB_DIR / "plant_terms.sqlite"
 CHAR_DB = VOCAB_DIR / "char_sub_matrix.sqlite"
 
+FULL_TAXON_DB = (VOCAB_DIR / "taxa.sqlite").absolute()
+# Some test taxa are in the term DB
+TAXON_DB = FULL_TAXON_DB if FULL_TAXON_DB.exists() else TERM_DB
+
 CURR_DIR = Path(os.getcwd())
 IS_SUBDIR = CURR_DIR.name in ("notebooks", "experiments")
 ROOT_DIR = Path("../.." if IS_SUBDIR else ".")
 
 DATA_DIR = ROOT_DIR / "data"
-
-FULL_TAXON_DB = DATA_DIR / "taxa.sqlite"
-# Some test data taxa is in the term DB
-TAXON_DB = FULL_TAXON_DB if FULL_TAXON_DB.exists() else TERM_DB
 
 TITLE_SHAPES = set(""" Xxxxx Xxxx Xxx Xx X. Xx. X """.split())
 UPPER_SHAPES = set(""" XXXXX XXXX XXX XX X. XX. X """.split())
