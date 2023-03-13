8f949b7c0124c1b5689994ff09ecc818c855b58e
rafelafrance <raphael.lafrance@gmail.com>
2023-03-13 12:41:43 -0400

    Update char_sub_matrix to use CSV instead of a DB

--- plants/pylib/char_sub_matrix.py
+++ plants/pylib/char_sub_matrix.py
@@ -1,11 +1,11 @@
-import sqlite3
+import csv
 
 from . import const
 
 
 def get_char_sub_scores():
-    with sqlite3.connect(const.CHAR_DB) as cxn:
-        cxn.row_factory = sqlite3.Row
-        rows = cxn.execute("select * from char_sub_matrix")
-        scores = {(r["char1"], r["char2"]): (r["sub"], r["score"]) for r in rows}
-    return scores
+    vocab_csv = const.VOCAB_DIR / "char_sub_matrix.csv"
+    with open(vocab_csv) as vocab_file:
+        reader = csv.DictReader(vocab_file)
+        rows = list(reader)
+    return {(r["char1"], r["char2"]): (r["sub"], r["score"]) for r in rows}
